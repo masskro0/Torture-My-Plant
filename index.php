4f56bde8736d50c716903e02d8c7ff1f1c2f229c
Michael_Heine <michael.heine1@gmx.net>
2019-06-12 18:58:43 +0200

    Loginfeature implementiert

--- index.php
+++ index.php
@@ -5,6 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Torture some plants. It's up to you how.</title>
     <link rel="stylesheet" type="text/css" href="styles/index.css">
+    <script>
+        /* Asynchronous call of the validation script login.php to display without refreshing the page. Refreshes the pages if everything is ok. */
+        function validate() {
+        var text;
+        var uname = document.getElementById('username');
+        var psw = document.getElementById('password');
+        var xmlhttp = new XMLHttpRequest();
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                window.text = this.responseText;
+                document.getElementById("error").innerHTML = this.responseText;
+                var str = this.responseText;
+                if(str.length ==0){ window.location.replace("index_torture.html");
+                }
+            }
+        };
+        xmlhttp.open("POST", "login.php", true);
+        xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xmlhttp.send("username=" + uname.value + "&password=" + psw.value);
+        }
+        
+    
+    </script>
   </head>
     <body>
     
@@ -41,28 +65,25 @@
 class="close" title="Close Modal">&times;</span>
 
   <!-- Modal Content -->
-  <form class="modal-content animate" action="login.php" method="POST">
-
+  <form class="modal-content animate" method="POST" action="" >
       <p class="logintext">Login</p>
     <div class="container">
+        <div class ="error" id="error"></div>
       <label for="username"><b>Username</b></label>
-      <input type="text" placeholder="Username..." name="username" required>
-
+      <input type="text" placeholder="Username..." name="username" id="username" required>
       <label for="password"><b>Password</b></label>
-      <input type="password" placeholder="Password..." name="password" required>
+      <input type="password" placeholder="Password..." name="password" id="password" required>
         <label>
             <input type="checkbox" checked="checked" name="remember"> Keep me logged in
         </label>
-      <input class="accept" class="loginbutton" type="submit" value="Login">
+      <input name="submit" type="button" class="accept loginbutton" value="Login" onclick="validate()" >
       
     </div>
   </form>
 </div>
-      
-    </body>
-    
-    
-    <script>
+</body>
+        
+<script>
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -73,6 +94,5 @@ window.onclick = function(event) {
   }
 }
 </script>
-    
-    
+
 </html>
--- register.php
+++ register.php
@@ -11,6 +11,7 @@ function test_input($data) {
 
 if(isset($_POST['submit'])) {
     session_start();
+    // Array to display errors
     $errors = array(); 
     // Login info for the MySQL database
     $host = 'localhost';
@@ -26,28 +27,23 @@ if(isset($_POST['submit'])) {
     // Check whether the username field is empty or not
     if(empty($_POST['username']) || !isset($_POST['username'])){
         array_push($errors, "Username is required");
-        #die('Please enter an username');
     }
     // Check whether the e-mail field is empty or not
     if(empty($_POST['email']) || !isset($_POST['email'])){
         array_push($errors, "E-mail is required");
-        #die('Please enter an e-mail adress');
     }
     // Check whether the first password field is empty or not
     if(empty($_POST['password1']) || !isset($_POST['password1'])){
         array_push($errors, "Password is required");
-        #die('Please enter a password');
     }
     // Check whether the second password field is empty or not
     if(empty($_POST['password2']) || !isset($_POST['password2'])){
         array_push($errors, "Please repeat your password");
-        #die('Please repeat your password');
     }
     // Validate user input
     // E-mail validation
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
         array_push($errors, "Invalid e-mail adress");
-	#die('Invalid e-mail adress');
     }
     else{
         $_POST['email'] = test_input($_POST['email']);
@@ -55,7 +51,6 @@ if(isset($_POST['submit'])) {
     // Validate username
     if (!preg_match('/[A-Za-z0-9]+/', $_POST['username'])) {
         array_push($errors, "Username is invalid");
-        #die('Invalid username. Please enter another one.');
     }
     else{
         $_POST['username'] = test_input($_POST['username']);
@@ -63,12 +58,10 @@ if(isset($_POST['submit'])) {
     //Check if both passwords are similar
     if($_POST['password1'] !== $_POST['password2']) {
         array_push($errors, "Passwords do not match");
-        #die('Passwords do not match');
     }
     // Check, if passwords have the right length
     if (strlen($_POST['password1']) > 30 || strlen($_POST['password1']) < 5) {
         array_push($errors, "Password must be between 5 and 30 characters long");
-	   #die('Password must be between 5 and 30 characters long!');
     }
     
     // Allow only JPEG format
@@ -76,16 +69,13 @@ if(isset($_POST['submit'])) {
     if(($verifyimg['mime'] != 'image/jpeg') && !(empty($_FILES['image']['tmp_name']))) {
         array_push($errors, "Only JPEG images are allowed");
     }   
-    
-    // todo Sanitize
-    
-    
+
     // Check if there is already an existing account with that username
     if ($stmt_username = $connect->prepare('SELECT user_id, password FROM User WHERE username = ?')) {
         $stmt_username->bind_param('s', $_POST['username']);
         $stmt_username->execute();
         $stmt_username->store_result();
-        //num_rows has to be 0 if it's a non existing username
+        // num_rows has to be 0 if it's a non existing username
         if ($stmt_username->num_rows > 0) {
             // Username already exists
             array_push($errors, "This username is already taken. Please choose another one.");
@@ -96,7 +86,7 @@ if(isset($_POST['submit'])) {
         $stmt_email->bind_param('s', $_POST['email']);
         $stmt_email->execute();
         $stmt_email->store_result();
-        //num_rows has to be 0 if it's a non existing e-mail
+        // num_rows has to be 0 if it's a non existing e-mail
         if ($stmt_email->num_rows > 0) {
             // E-mail already exists
             array_push($errors, "This email is already taken. Please choose another one.");
@@ -105,7 +95,7 @@ if(isset($_POST['submit'])) {
     
     // Insert new account
     if($stmt_username->num_rows === 0 && $stmt_email->num_rows === 0 && count($errors) == 0) {        
-        //Get user id
+        // Get user id
         $sql = "SELECT user_id FROM User ORDER BY user_id DESC LIMIT 0, 1";
         $lastuserid = 0;
         $result = mysqli_query($connect, $sql);
