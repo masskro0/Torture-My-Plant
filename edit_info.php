a2fbfa7faa94ffbd671f4409cb0015e2e9431f1c
Michael_Heine <michael.heine1@gmx.net>
2019-07-17 16:57:03 +0200

    Ignore

--- edit_info.php
+++ edit_info.php
@@ -168,7 +168,7 @@ if(isset($_POST['submit'])) {
 
         // Check if the two new passwords are the same
         if($_POST['password_new1'] !== $_POST['password_new2']) {
-                array_push($errors, "The new passwords do not match.");
+            array_push($errors, "The new passwords do not match.");
         }
 
         // Check, if passwords have the right length
